a77e00da035540a15f0f0a5ad2d15994d904f066
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-08-06 16:01:59 +0300

    advanced Calculator

--- Calculator_cpp_oop/Calculator_cpp_oop.cpp
+++ Calculator_cpp_oop/Calculator_cpp_oop.cpp
@@ -1,60 +1,93 @@
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class clsCalculator {
 private:
-	double _num = 0;
-	double InputNum = 0;
-	string _role = "Add";
+	double _result = 0;
+	double _LastNumber = 0;
+	double _perviseResult = 0;
+	string _LastOperation = "Add";
+	string _PreviousOperation = "Add";
+	vector<string> _PreviousOperations;
+
+	void _ChangeOperation(string Operation) {
+		_PreviousOperation = _LastOperation;
+		_PreviousOperations.push_back(Operation);
+		_LastOperation = Operation;
+	}
+
+
 
 public:
+	void CancelLastOperation() {
+		_PreviousOperations.pop_back();
+		_LastOperation = _PreviousOperation;
+		_PreviousOperation = _PreviousOperations[_PreviousOperations.size() - 1];
+	}
 	void PrintResult() {
-		cout << "Result After " << _role << " " << InputNum << " is:" << _num << endl;
+		cout << "Result After " << _LastOperation << " " << _LastNumber << " is:" << _result << endl;
 	}
 	void SetNumber(double Num) {
-		_num = Num;
-		_role = "SetNumber";
+		_LastNumber = Num;
+		_perviseResult = _result;
+		_result = Num;
+		_ChangeOperation("SetNumber");
 	}
 	void Clear() {
-		InputNum = _num;
-		_num = 0;
-		_role = "Clear";
+		_LastNumber = 0;
+		_perviseResult = _result;
+		_result = 0;
+		_ChangeOperation("Clear");
 	}
 	void Add(double Num) {
-		_num += Num;
-		InputNum = Num;
-		_role = "Add";
+		_LastNumber = Num;
+		_perviseResult = _result;
+		_result += Num;
+		_ChangeOperation("Add");
 	}
 	void Subtract(double Num) {
-		_num -= Num;
-		InputNum = Num;
-		_role = "Subtract";
+		_LastNumber = Num;
+		_perviseResult = _result;
+		_result -= Num;
+		_ChangeOperation("Subtract");
 	}
 	void Multiply(double Num) {
-		_num *= Num;
-		InputNum = Num;
-		_role = "Multiply";
+		_LastNumber = Num;
+		_perviseResult = _result;
+
+		_result *= Num;
+		_ChangeOperation("Multiply");
 	}
 	void Divede(double Num) {
-		_num /= Num;
-		InputNum = Num;
-		_role = "Divede";
+		_LastNumber = Num;
+		_perviseResult = _result;
+		if (Num == 0) {
+			cout << "YOU CAN'T DIVIDE ON ZERO" << endl;
+		}
+		else {
+			_result /= Num;
+		}
+		_ChangeOperation("Divede");
 	}
 	void Mod(double Num) {
-		_num = (int)_num % (int)Num;
-		InputNum = Num;
-		_role = "Mod";
+		_LastNumber = Num;
+		_perviseResult = _result;
+		_result = (int)_result % (int)Num;
+		_ChangeOperation("Mod");
 	}
 	void Pow(double Num) {
-		_num = pow(_num, Num);
-		InputNum = Num;
-		_role = "Pow";
+		_LastNumber = Num;
+		_perviseResult = _result;
+		_result = pow(_result, Num);
+		_ChangeOperation("Pow");
 	}
 	void Sqrt() {
-		InputNum = _num;
-		_num = sqrt(_num);
-		_role = "Sqrt";
+		_LastNumber = _result;
+		_perviseResult = _result;
+		_result = sqrt(_result);
+		_ChangeOperation("Sqrt");
 	}
 
 };
@@ -95,5 +128,8 @@ int main()
 	Calculator.PrintResult();
 
 	Calculator.Sqrt();
+	Calculator.PrintResult();
+
+	Calculator.CancelLastOperation();
 	Calculator.PrintResult();
 }
